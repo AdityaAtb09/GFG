bee8f8e01ef317964dd337631cfb92f88b51930d
Aditya Atbhaiya <126330557+AdityaAtb09@users.noreply.github.com>
2024-02-29 10:40:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    
+    int firstOcc(int arr[], int n, int x)
+    {
+        int s = 0, e = n-1;
+        int mid = s+(e-s)/2;
+        int ans = -1;
+        
+        while(s <= e)
+        {
+            if(arr[mid] == x)
+            {
+                ans = mid;
+                e = mid-1;
+            }
+            else if(arr[mid] > x)
+            {
+                e = mid-1;
+            }
+            else
+            {
+                s = mid+1;
+            }
+            
+            mid = s+(e-s)/2;
+        }
+        
+        return ans;
+    }
+    
+    int lastOcc(int arr[], int n, int x)
+    {
+        int s = 0, e = n-1;
+        int mid = s+(e-s)/2;
+        int ans = -1;
+        
+        while(s <= e)
+        {
+            if(arr[mid] == x)
+            {
+                ans = mid;
+                s = mid+1;
+            }
+            else if(arr[mid] > x)
+            {
+                e = mid-1;
+            }
+            else
+            {
+                s = mid+1;
+            }
+            
+            mid = s+(e-s)/2;
+        }
+        
+        return ans;
+    }
+    
+    vector<int> find(int arr[], int n , int x )
+    {
+        // code here
+        vector<int> ans;
+        ans.push_back(firstOcc(arr, n, x));
+        ans.push_back(lastOcc(arr, n, x));
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        Solution ob;
+        ans=ob.find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
